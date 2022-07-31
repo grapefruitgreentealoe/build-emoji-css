c5a83707275f1cf890aeef8ac6773001a6c7c260
eunsun <nammugmuhg@gmail.com>
2022-08-01 03:19:02 +0900

    feat : mkdir and write or appen file with recursive option true

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const emoji = require("./generate").emoji;
 const Mustache = require("mustache");
 const fs = require("fs");
-const mkdirp = require("mkdirp");
 const path = require("path");
 
 async function write(emojis) {
   let filename = path.join(__dirname, "..", "/sass/emojis/_all.scss");
   let dirname = path.join(__dirname, "..", "/sass/emojis");
-  if (!fs.existsSync(dirname)) await mkdirp(dirname);
+  if (!fs.existsSync(dirname))
+    fs.mkdirSync(dirname, { recursive: true }, (err) => {});
   let items = Object.keys(emojis);
   save_template(
     filename,
@@ -20,7 +20,8 @@ async function write(emojis) {
 
   items.forEach(async (element) => {
     dirname = path.join(__dirname, "..", "/sass/emojis/", element);
-    if (!fs.existsSync(dirname)) await mkdirp(dirname);
+    if (!fs.existsSync(dirname))
+      fs.mkdirSync(dirname, { recursive: true }, (err) => {});
     emojis[element].forEach(({ type, unicode, id }) => {
       filename = path.join(
         __dirname,
@@ -45,11 +46,11 @@ function save_template(filename, template, items) {
   const template_text = Mustache.render(template, items);
   console.log(template_text);
   if (!fs.existsSync(filename)) {
-    fs.writeFile(filename, template_text, (err) => {
+    fs.writeFileSync(filename, template_text, { recursive: true }, (err) => {
       if (err) throw err;
     });
   } else {
-    fs.appendFile(filename, template_text, (err) => {
+    fs.appendFileSync(filename, template_text, { recursive: true }, (err) => {
       if (err) throw err;
     });
   }
